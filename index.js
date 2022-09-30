ea26835c137ba7af30c12e1b4e1e03c82e5c4504
Dmitriy <xxlaimxx1999@gmail.com>
2022-09-30 12:17:05 +0300

    fix:back

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use("/api", noteRouter);
-app.use(cors({}));
+app.use("/", noteRouter);
+
+// app.use(cors({}));
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
