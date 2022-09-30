13b372fa3765f4257b952107b9318ec3d38c5bbf
Dmitriy <xxlaimxx1999@gmail.com>
2022-09-30 16:37:52 +0300

    fix:error

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", noteRouter);
-app.use("/", app.response.send("App work"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
