1e629754b49f7ad68f120bff318e67c57812ed21
Dmitriy <xxlaimxx1999@gmail.com>
2022-09-30 13:46:54 +0300

    fix: inex

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-app.use("/", noteRouter);
+app.use(cors());
+app.use("/api", noteRouter);
 
-console.log(process.env.POSTGRES_HOST);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
