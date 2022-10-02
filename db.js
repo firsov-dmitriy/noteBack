481b615d9f8b1de8745e2f859dd457df69d375dc
Dmitriy <xxlaimxx1999@gmail.com>
2022-10-02 11:51:05 +0300

    fix : db

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
-});
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   host: process.env.POSTGRES_HOST,
+//   port: process.env.POSTGRES_PORT,
+//   database: process.env.POSTGRES_DB,
+// });
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-// const pool = new Pool({
-//   connectionString,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
